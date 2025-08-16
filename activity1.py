e5b0147deff62185ace8d0eb81806cf5ed9666ec
Kushagraa01 <[swatiabhijain@gmail.com]>
2025-08-16 20:36:50 +0530

    lesson

--- activity1.py
+++ activity1.py
@@ -6,3 +6,9 @@ print("By using 'NOT' operater on secound value :", ~num2)
 print("By using 'AND' operater ,first value & secound value :", num1 & num2)
 print("By using 'OR' operater ,first value | secound value :", num1 | num2)
 print("By using 'XOR' operater ,first value ^ secound value :", num1 ^ num2)
+
+print("By using 'Right shift' operater on first value :", num1 >> 1)
+print("By using 'Right shift' operater on secound value :", num2 >> 1)
+
+print("By using 'left shift' operater on first value :", num1 << 1)
+print("By using 'left shift' operater on secound value :", num2 << 1)
